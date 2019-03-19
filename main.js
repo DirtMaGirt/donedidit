c0f41f02cc8edf78319bf8b50fdc4137d0a8b53b
Jamal Ames <Jamal.ames@gmail.com>
2019-03-19 12:47:50 -0400

    pushing because its been awhile. no traction has been made.

--- main.js
+++ main.js
@@ -9,7 +9,6 @@ For example, isDone[3] would hold the "done-ness" information for todos[3].
 
 let todos = [];
 let isDone = [];
-let completed = [];
 
 // When the html finishes loading, launch `init`.
 window.onload = init;
@@ -27,6 +26,7 @@ function init() {
     document.querySelector('#clear-all-todos')
         .addEventListener('click', clearAllTodos)
 
+
 }
 
 function addTodo(event) {
@@ -40,21 +40,20 @@ function addTodo(event) {
     document.querySelector('#new-todo').value = '';
     // Put the todo and its "done-ness" in their respective arrays.
     todos.push(listItem)
-    isDone.push(listItem)
+    isDone.push('False')
     console.log(isDone)
+    console.log(todos)
 
     // Create a new html element and put our new todo's text in there.
-    const newlI = document.createElement('li');
-    newlI.innerText = listItem;
+    const newLi = document.createElement('li');
+    newLi.innerText = listItem;
+
     // Add an event listener on the newly created html element to launch
     // `toggleDone` when it's clicked.
 
-
-    document.querySelector('#todo-list')
-        .addEventListener('click', toggleDone);
     // Put our new element on the list part of our page!
     const oL = document.querySelector('#todo-list')
-    oL.appendChild(newlI);
+    oL.appendChild(newLi);
 
 }
 
@@ -115,49 +114,35 @@ function toggleDone(event) {
     // Grab the HTML element that was clicked.
     // If you don't know, the event parameter has what you need... somewhere.
 
-    const toggledItem = document.querySelector("#todo-list");
-    let completedTask = toggledItem.getElementsByTagName("LI");
-    for (let i - 0; i < completedTask)
-
-        currentTarget.style.visibility = 'hidden'
-    completed.push(completedTask);
-    console.log(completed)
+    const toggledItem = document.getElementById("#todo-list");
+    toggleItem.getElementsByTagName("LI")[1].style.backgroundColor = "red";
+    // Find the index of the array that this todo resides in. There are a couple
+    // ways to do this, and I'm sure you'll figure one out!
 
 
+    // *IF* it's not done yet, apply strikethrough. Otherwise, take that
+    // strikethrough away!
 
 
+    // Toggle the "done-ness" of the same todo, using the isDone array.
 
-    for (var i = 0; i < ps.length; i++) {
-        // Console: print the clicked <p> element 
-        ps[i].addEventListener('click', hide, false);
+}
 
-        // Find the index of the array that this todo resides in. There are a couple
-        // ways to do this, and I'm sure you'll figure one out!
-
-
-        // *IF* it's not done yet, apply strikethrough. Otherwise, take that
-        // strikethrough away!
+function removeAllChildrenOfOl() {
+    // Grab the ol.
 
 
-        // Toggle the "done-ness" of the same todo, using the isDone array.
+    // Remove all its children.
+    // The way I like to do that is to continue to remove children as long as
+    // there are some to remove.
+    // Look at the methods `.hasChildNodes` and `removeChild`.
+    // There are other ways too, though. Feel free to poke around.
 
+}
+
+function resetAllInputs() {
+    const inputs = document.querySelectorAll('Input');
+    for (let i = 0; i < inputs.length; i++) {
+        inputs[i].value = '';
     }
-
-    function removeAllChildrenOfOl() {
-        // Grab the ol.
-
-
-        // Remove all its children.
-        // The way I like to do that is to continue to remove children as long as
-        // there are some to remove.
-        // Look at the methods `.hasChildNodes` and `removeChild`.
-        // There are other ways too, though. Feel free to poke around.
-
-    }
-
-    function resetAllInputs() {
-        const inputs = document.querySelectorAll('Input');
-        for (let i = 0; i < inputs.length; i++) {
-            inputs[i].value = '';
-        }
-    }
+}
